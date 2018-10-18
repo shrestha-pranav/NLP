8797ddaa0ed4697d28e549fb31e5242f3b59efce
shrestha-pranav <shresthapranav278@gmail.com>
2018-10-17 21:58:58 -0400

    Complete hw2

--- hw2/grammar.py
+++ hw2/grammar.py
@@ -43,15 +43,27 @@ class Pcfg(object):
         return (lhs, rhs, prob)
 
     def verify_grammar(self):
-        """
-        Return True if the grammar is a valid PCFG in CNF.
-        Otherwise return False. 
-        """
-        # TODO, Part 1
-        return False 
+        for key in self.lhs_to_rules:
+            # Check if probability sums to 1
+            sum_prob = math.fsum(x[2] for x in grammar.lhs_to_rules[key])
+            if abs(sum_prob - 1) > 0.00001:
+                return False
+            
+            # Check if non-terminals are all capital
+            for rule in self.lhs_to_rules[key]:
+                rhs = rule[1]
+                if len(rhs)> 1:
+                    for v in rhs:
+                        if not v.isupper():
+                            return False
+            
+        return True
 
 
 if __name__ == "__main__":
     with open(sys.argv[1],'r') as grammar_file:
         grammar = Pcfg(grammar_file)
-        
+        if grammar.verify_grammar():
+            print("Grammar is valid PCFG in CNF")
+        else:
+            print("Grammar is not valid.")        
